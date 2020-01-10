3deaf208727551f0c0793db129da52a6f5db476c
kdord <katya.koliada49@gmail.com>
2020-01-10 19:17:56 +0200

    refactor styling

--- src/Palette.js
+++ src/Palette.js
@@ -33,7 +33,7 @@ export default class Palette extends Component {
         name={color.name}
         key={color.id}
         moreUrl={`/palette/${id}/${color.id}`}
-        showLink={true}
+        showingFullPalette
       />
     ));
     return (
