3b2bc9cefe6e2945a48c5a7973cbd2f170aec09c
kdord <katya.koliada49@gmail.com>
2020-01-10 13:41:17 +0200

    start styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,23 +2,43 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-  main: {
-    backgroundColor: 'pink',
-    border: '3px solid teal',
-    '& h1': {
-      color: 'white'
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer'
     }
   },
-  secondary: {
-    backgroundColor: 'blue'
+  colors: {
+    backgroundColor: 'gray'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem'
   }
 };
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   return (
-    <div className={classes.main}>
-      <h1>mini palette</h1>
-      <section className={classes.secondary}>mini palette</section>
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>
+        {paletteName} <span className='classes.emoji'>{emoji}</span>
+      </h5>
     </div>
   );
 }
