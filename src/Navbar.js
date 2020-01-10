9222dc7ee740f57c8e7d2c589853f2d604a3268f
kdord <katya.koliada49@gmail.com>
2020-01-10 20:45:24 +0200

    move out Navbar styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,57 +7,9 @@ import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import { IconButton } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-// import styles from './styles/NavbarStyles';
+import styles from './styles/NavbarStyles';
 import { withStyles } from '@material-ui/styles';
 
-const styles = {
-  Navbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: '6vh'
-  },
-  logo: {
-    marginRight: '15px',
-    padding: '0 13px',
-    fontSize: '22px',
-    backgroundColor: '#eceff1',
-    fontFamily: 'Roboto',
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-
-    '& a': {
-      textDecoration: 'none',
-      color: 'black'
-    }
-  },
-  slider: {
-    width: '350px',
-    margin: '0 20px',
-    display: 'inline-block',
-
-    '& .rc-slider-track': {
-      backgroundColor: 'transparent'
-    },
-    '& .rc-slider-rail': {
-      height: '8px'
-    },
-    '& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover': {
-      backgroundColor: 'green',
-      outline: 'none',
-      border: '2px solid green',
-      boxShadow: 'none',
-      width: '13px',
-      height: '13px',
-      marginTop: '-3px'
-    }
-  },
-  selectContainer: {
-    marginLeft: 'auto',
-    marginRight: '1rem'
-  }
-};
 class Navbar extends Component {
   constructor(props) {
     super(props);
