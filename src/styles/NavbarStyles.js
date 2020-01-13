6040eda00377a302fa3f97a41ae10d0e80063973
kdord <katya.koliada49@gmail.com>
2020-01-13 16:36:07 +0200

    make form and navbar responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 export default {
   Navbar: {
     display: 'flex',
@@ -18,6 +19,9 @@ export default {
     '& a': {
       textDecoration: 'none',
       color: 'black'
+    },
+    [sizes.down('sm')]: {
+      fontSize: '16px'
     }
   },
   slider: {
@@ -39,6 +43,9 @@ export default {
       width: '13px',
       height: '13px',
       marginTop: '-3px'
+    },
+    [sizes.down('md')]: {
+      width: '150px'
     }
   },
   selectContainer: {
--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,4 +1,5 @@
 import { DRAWER_WIDTH } from '../constants';
+import sizes from './sizes';
 
 const drawerWidth = DRAWER_WIDTH;
 const styles = theme => ({
@@ -33,13 +34,21 @@ const styles = theme => ({
     marginRight: 20
   },
   navBtns: {
-    marginRight: '1rem',
+    marginRight: '0.3rem',
     '& a': {
       textDecoration: 'none'
+    },
+    [sizes.down('xs')]: {
+      marginRight: '0.5rem'
     }
   },
   button: {
-    margin: '0 0.5rem'
+    margin: '0 0.5rem',
+
+    [sizes.down('xs')]: {
+      margin: '0 0.2rem',
+      padding: '0.2rem 0.3rem'
+    }
   }
 });
 
